9bc9f4aba90b5961f2990b676f2a9fd4b9f8c9cb
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-07-31 01:11:38 -0700

    completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_144445) do
-
-  create_table "spells", force: :cascade do |t|
-    t.string "name"
-    t.integer "level"
-    t.string "description"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
